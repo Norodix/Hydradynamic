8a644db2a7fba310b5b4152d5efea1bc7d28f555
Norodix <peceli.bence@gmail.com>
2023-05-10 09:40:48 +0200

    Cable copy parent power if not direct connected

--- Source/Props/Cable.gd
+++ Source/Props/Cable.gd
@@ -3,10 +3,21 @@ extends MeshInstance3D
 @export_node_path("Area3D") var connection
 
 func _process(delta):
-	var conn = get_node(connection)
+	var conn = null
+	if connection:
+		conn = get_node_or_null(connection)
 	var mat = get_active_material(0)
 	if not mat:
 		return
+	
+	# Use parent by default
+	# Use connected node if present
+	if not conn:
+		conn = get_parent()
+	if not conn:
+		# something gone bad
+		return
+		
 	if conn.has_method("is_powered"):
 		var power = conn.is_powered()
 		var electricity_pass = mat.next_pass.next_pass
