c7b11e37e86a99246ae1e09af7d0e9848d4ed79a
Norodix <peceli.bence@gmail.com>
2023-05-11 21:27:06 +0200

    Add laser scanner to airlock

--- Source/Props/head_count_scanner.gd
+++ Source/Props/head_count_scanner.gd
@@ -6,8 +6,11 @@ signal activate
 
 @onready var img : Sprite3D = $Sprite3D
 @onready var txt : Label3D = $Label3D
+@onready var anim : AnimationPlayer = $Scanner/AnimationPlayer
+
 func _ready():
 	$Label3D.text = str(head_count) + "x"
+	await get_tree().process_frame
 	var aabb_img = img.get_aabb()
 	var aabb_txt = txt.get_aabb()
 	var lp = aabb_img.size.x * img.transform.basis.get_scale().x
@@ -18,6 +21,12 @@ func _ready():
 
 
 func _on_area_3d_body_entered(body):
+	if anim.is_playing():
+		return
+	anim.play("Scan")
+	await anim.animation_finished
+	anim.play("RESET")
+	
 	if body.has_method("get_head_count"):
 		var body_headcount = body.get_head_count()
 		print(body_headcount, " vs ", head_count)
