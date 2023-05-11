a1a72260d25453cb8e25d3d05a25f7bee62841de
Norodix <peceli.bence@gmail.com>
2023-05-11 10:26:09 +0200

    Add world settings applier node

---
+++ Source/Environment/world_settings.gd
@@ -0,0 +1,6 @@
+extends Node3D
+
+func _ready():
+	var viewport = get_viewport()
+	viewport.screen_space_aa = Viewport.SCREEN_SPACE_AA_FXAA if Globals.fxaa_enabled else Viewport.SCREEN_SPACE_AA_DISABLED
+	
