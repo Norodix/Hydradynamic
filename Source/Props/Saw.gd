4dd6ecfaf4665bf25164f74ed5e00b71f457afc2
Norodix <peceli.bence@gmail.com>
2023-05-12 23:05:22 +0200

    Add saw audio

--- Source/Props/Saw.gd
+++ Source/Props/Saw.gd
@@ -3,17 +3,20 @@ extends Node3D
 
 @onready var anim : AnimationPlayer = $AnimationPlayer
 @onready var cutter : CollisionShape3D = $Saw_Blade/Area3D/CollisionShape3D
+@onready var sound : AudioStreamPlayer3D = $AudioStreamPlayer3D
 var broken = false
 
 
 func activate():
 	if not broken:
 		anim.play("Spin")
+		rampup_sound()
 		cutter.disabled = false
 
 
 func deactivate():
 	cutter.disabled = true
+	rampdown_sound()
 	anim.pause()
 
 
@@ -27,3 +30,19 @@ func hit():
 	cutter.disabled = true
 	print("Hit called")
 	anim.play("Break")
+	rampdown_sound()
+
+
+func rampup_sound():
+	sound.pitch_scale = 0.3
+	sound.volume_db = -60
+	sound.play()
+	var sound_tween = get_tree().create_tween()
+	sound_tween.tween_property(sound, "pitch_scale", 1, 1)
+	sound_tween.parallel().tween_property(sound, "volume_db", -20, 1)
+
+
+func rampdown_sound():
+	var sound_tween = get_tree().create_tween()
+	sound_tween.tween_property(sound, "pitch_scale", 0.1, 1)
+	sound_tween.parallel().tween_property(sound, "volume_db", -60, 1)
