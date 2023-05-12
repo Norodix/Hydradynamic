f40defbc06c037a74913c9770836920603f389d5
Norodix <peceli.bence@gmail.com>
2023-05-12 16:59:12 +0200

    Add flame audio

--- Source/Props/burner_tube.gd
+++ Source/Props/burner_tube.gd
@@ -1,5 +1,15 @@
 extends Node3D
 
+@onready var particle = $FireParticles3D
+@onready var audio = $AudioStreamPlayer3D
+
+func _process(delta):
+	if particle.emitting:
+		if not audio.playing:
+			audio.play()
+	else:
+		audio.stop()
+
 
 func start():
 	print("Start emit")
