e8c2fa680f5e0a3d4d20eff86805e8b48b2b76cf
Norodix <peceli.bence@gmail.com>
2023-05-13 09:03:13 +0200

    Add fan audio

--- Source/Props/Fan.gd
+++ Source/Props/Fan.gd
@@ -2,30 +2,38 @@ extends StaticBody3D
 
 
 @onready var anim : AnimationPlayer = $AnimationPlayer_Fan
-@onready var anim2 : AnimationPlayer = $AnimationPlayer_Grid
+@onready var anim_grid : AnimationPlayer = $AnimationPlayer_Grid
 @onready var cutter : CollisionShape3D = $Area3D/CollisionShape3D
+@onready var sound : AudioStreamPlayer3D = $AudioStreamPlayer3D_running
+@onready var sound_grid : AudioStreamPlayer3D = $AudioStreamPlayer3D_grid
+@onready var sound_crash : AudioStreamPlayer3D = $AudioStreamPlayer3D_crash
 var broken = false
 
 
 func _ready():
 		anim.play("Spin")
 		cutter.disabled = true
+		rampup_sound()
 
 
 func activate():
 	if not broken:
 		cutter.disabled = false
-		anim2.play("Open")
+		anim_grid.play("Open")
+		sound_grid.play()
+
 
 func deactivate():
 	if not broken:
 		cutter.disabled = true
-		anim2.play_backwards("Open")
+		anim_grid.play_backwards("Open")
+		sound_grid.play()
 
 
 func reset_level():
 	cutter.disabled = true
 	broken = false
+	_ready()
 
 
 func hit():
@@ -33,4 +41,18 @@ func hit():
 	cutter.disabled = true
 	print("Hit called")
 	anim.play("Break")
-	anim2.play_backwards("Open")
+	anim_grid.play_backwards("Open")
+	sound_grid.play()
+	sound_crash.play()
+	sound.stop()
+	
+
+
+func rampup_sound():
+	sound.pitch_scale = 0.3
+	sound.volume_db = -60
+	sound.play()
+	var sound_tween = get_tree().create_tween()
+	sound_tween.tween_property(sound, "pitch_scale", 1, 1)
+	sound_tween.parallel().tween_property(sound, "volume_db", -20, 1)
+
