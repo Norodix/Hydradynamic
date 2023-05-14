f196c8fde0c440173fa433284fb41478d927ec43
Norodix <peceli.bence@gmail.com>
2023-05-14 15:09:41 +0200

    Add powered wall light

---
+++ Source/Props/Wall_Light.gd
@@ -0,0 +1,25 @@
+extends Node3D
+
+@export_node_path("Area3D") var connection
+@export var is_powered = true
+
+func _process(delta):
+	var conn = null
+	if connection:
+		conn = get_node_or_null(connection)
+	var mat = get_child(0).get_active_material(0)
+	if not mat:
+		return
+	
+	# Use parent by default
+	# Use connected node if present
+	if not conn:
+		conn = get_parent()
+	if not conn:
+		# something gone bad
+		return
+		
+	if conn.has_method("is_powered"):
+		is_powered = conn.is_powered()
+	
+	$OmniLight3D.visible = is_powered
