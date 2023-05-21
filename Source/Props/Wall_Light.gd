675330959c7e970b8d3e9a37919615d08a866891
bbluntblade <barna.bertalan95@gmail.com>
2023-05-21 22:12:03 +0200

    Wall light, dimming

--- Source/Props/Wall_Light.gd
+++ Source/Props/Wall_Light.gd
@@ -2,7 +2,8 @@ extends Node3D
 
 @export_node_path("Area3D") var connection
 @export var is_powered = true
-
+@onready var light = $OmniLight3D
+@onready var mat = $Wall_Light2.get_surface_override_material(2)
 
 func turn_on():
 	is_powered = true
@@ -31,6 +32,10 @@ func _process(delta):
 	if conn.has_method("is_powered"):
 		is_powered = conn.is_powered()
 	
-	$OmniLight3D.visible = is_powered
-	$Wall_Light2.get_surface_override_material(2).emission_enabled = is_powered
+func _physics_process(delta):
+	if (light.light_energy < 0.1):
+		light.visible = is_powered
+		mat.emission_enabled = is_powered
+	light.light_energy = lerp(light.light_energy, float(is_powered), 0.1)
+	mat.emission_energy_multiplier = lerp(mat.emission_energy_multiplier, float(is_powered) * 3.0, 0.1)
 	
